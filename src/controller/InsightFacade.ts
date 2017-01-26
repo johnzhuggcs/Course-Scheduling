4163b374e5668a5f32f8a3b05e7febd1767c4760
johnzhuggcs <johnzhu73@yahoo.com>
2017-01-25 21:31:56 -0800

    Week 1 Log: implemented interface for query syntax. Implemented isValid(), hasFilter(), hasArrayFilter() method that checks for syntax. Currently only able to check for the deepest level of multiple nested FILTERs, and skips directly to OPTIONS.

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,9 +1,11 @@
 /**
  * This is the main programmatic entry point for the project.
  */
-import {IInsightFacade, InsightResponse, QueryRequest} from "./IInsightFacade";
+import {IInsightFacade, InsightResponse, QueryRequest, FilterQuery} from "./IInsightFacade";
 
 import Log from "../Util";
+import {isString} from "util";
+import {isNumber} from "util";
 
 export default class InsightFacade implements IInsightFacade {
 
@@ -115,6 +117,109 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     isValid(query:QueryRequest):boolean{
-        return false;
+        //checks for query provided is of valid syntax
+        var checked;
+        var keyArray = Object.keys(query); //an array of the keys, should only be WHERE and OPTIONS now
+        var Where    //returns WHERE
+        var Options; //returns OPTIONS
+        var filter;  //returns value of FILTER
+        var optionsValue; //returns value of OPTIONS
+        var columnsEtcKey; //returns values including and after COLUMNS
+        var columnsValidKeyArray; //returns the array of Valid Keys assigned to COLUMNS
+        var orderValidKey; //returns the single valid key assigned to ORDER
+        var Table; //returns TABLE from VIEW
+        if(keyArray[0] == "WHERE" && keyArray[1] == "OPTIONS"){ //checks if outermost keys are WHERE and OPTIONS
 
-    }}
+                Where = keyArray[0]; //gets "WHERE"
+                Options = keyArray[1]; //gets"OPTIONS"
+                filter = query[Where]; //returns content of FILTER
+
+
+                if(this.hasFilter(filter)) { //check if FILTER is valid, needed as FILTER is recursively nested
+                    optionsValue = query[Options];
+                    columnsEtcKey = Object.keys(optionsValue);
+                    if(columnsEtcKey.length == 3 && columnsEtcKey[0] == "COLUMNS" && columnsEtcKey[1] == "ORDER" && columnsEtcKey[2] == "FORM"){
+                        columnsValidKeyArray = optionsValue[columnsEtcKey[0]]
+                        for(let x in columnsValidKeyArray){
+                            if(columnsValidKeyArray[x] == "courses_dept" || columnsValidKeyArray[x] == "courses_id"
+                                || columnsValidKeyArray[x] == "courses_avg" || columnsValidKeyArray[x] == "courses_instructor"
+                                || columnsValidKeyArray[x] == "courses_title" || columnsValidKeyArray[x] == "courses_pass"
+                                || columnsValidKeyArray[x] == "courses_fail" || columnsValidKeyArray[x] == "courses_audit"
+                                || columnsValidKeyArray[x] == "courses_uuid"){
+                                Where = keyArray[0] //dummy line of code so further check would be done outside of for-loop
+                            } else return false
+                        }
+                        orderValidKey = optionsValue[columnsEtcKey[1]];
+                        if(orderValidKey == "courses_dept" || orderValidKey == "courses_id"
+                            || orderValidKey == "courses_avg" || orderValidKey == "courses_instructor"
+                            || orderValidKey == "courses_title" || orderValidKey == "courses_pass"
+                            || orderValidKey == "courses_fail" || orderValidKey == "courses_audit"
+                            || orderValidKey == "courses_uuid"){
+                                Table = optionsValue[columnsEtcKey[2]];
+                                if(Table == "TABLE"){
+                                    checked = true;
+                                    if(checked == true){
+                                        return true
+                                    }
+                                }else return false;
+                        }
+                    } else return false;
+                } else return false;
+
+
+        }else return false;
+
+    }
+
+    hasFilter(filter:FilterQuery):boolean{ //
+        var checked;
+        var comparisonKey = Object.keys(filter); //gets first comparator from FILTER
+        var comparisonValue = filter[comparisonKey[0]] //gets value from each FILTER
+        var validProjectKey; //gets valid key e.g. courses_dept
+        var mComparisonNumber; //gets value number from MComparison key/value pair
+        var sComparisonString; //gets value string from SComparison key/value pair
+        if(comparisonKey.length == 1){ //checks that there is only one comparator
+
+            if(comparisonKey[0] == "AND" || comparisonKey[0] == "OR"){
+                if(this.hasArrayFilter(comparisonValue)){
+                    return true;
+                } else return false;
+
+            } else if(comparisonKey[0] == "LT" || comparisonKey[0] == "GT" || comparisonKey[0] == "EQ"){
+                validProjectKey = Object.keys(comparisonValue);
+                mComparisonNumber = comparisonValue[validProjectKey[0]];
+                if(validProjectKey.length == 1 && (validProjectKey[0] == "courses_avg" || validProjectKey[0] == "courses_pass" ||
+                    validProjectKey[0] == "courses_fail" || validProjectKey[0] == "courses_audit")){
+                        if (isNumber(mComparisonNumber)){
+                            return true;
+                        }else return false;
+                }else return false;
+
+            } else if (comparisonKey[0] == "IS"){
+                validProjectKey = Object.keys(comparisonValue);
+                sComparisonString = comparisonValue[validProjectKey[0]];
+                if(validProjectKey.length == 1  && (validProjectKey[0] == "course_dept" || validProjectKey[0] == "course_id"|| validProjectKey[0] == "courses_instructor"||validProjectKey[0] == "courses_title" || validProjectKey[0] == "courses_uuid")){
+                    if(isString(sComparisonString)||(sComparisonString.toString().charAt(0) && sComparisonString.toString().charAt(sComparisonString.toString().length - 1) &&
+                        isString(sComparisonString))){
+                        return true;
+                    }else return false;
+                }return false;
+
+            } else if (comparisonKey[0] == "NOT"){
+                this.hasFilter(comparisonValue);
+            } else return false
+        }else return false
+    }
+
+    hasArrayFilter(filterArray:FilterQuery[]):boolean{
+
+            for(let x in filterArray){
+                if(this.hasFilter(filterArray[x])) {//checks if each element is actually FILTER
+                    return true
+                }
+            }
+
+
+    }
+
+}
