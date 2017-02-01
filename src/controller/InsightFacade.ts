ccba6dd3a3081a25bf50943b7abd19766a8f0671
johnzhuggcs <johnzhu73@yahoo.com>
2017-01-31 18:14:49 -0800

    merging Pauls completed addDataset()

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -179,6 +179,8 @@ export default class InsightFacade implements IInsightFacade {
 
         if(this.isValid(query) == true){
             return null;
+
+
         }else
         return null;
         //perform query
