15f5c83fbd9a0a6cb0909ae12166b7564245c8b9
johnzhuggcs <johnzhu73@yahoo.com>
2017-01-31 21:04:43 -0800

    renamed QueryTest to QueryTestSpec

--- test/EchoSpec.ts
+++ test/EchoSpec.ts
@@ -79,7 +79,7 @@ describe("EchoSpec", function () {
 
 
     const fs = require('fs');
-    it.only("addDataset should add a dataset to UBCInsight", function () {
+    it("addDataset should add a dataset to UBCInsight", function () {
         Log.info("readFile:"+ fs.readFileSync('multi_courses.zip').toString('base64'));
         return insight.addDataset('UBCInsight',fs.readFileSync('multi_courses.zip').toString('base64')).then(function (value: InsightResponse) {
             var ir: InsightResponse;
@@ -92,7 +92,7 @@ describe("EchoSpec", function () {
 
     });
 
-    it.only("addDataset should add a dataset to UBCInsight", function () {
+    it("addDataset should add a dataset to UBCInsight", function () {
         Log.info("readFile:"+ fs.readFileSync('multi_courses.zip').toString('base64'));
         fs.writeFile('UBCInsight', '{}', (err: Error) => {
             if (err) throw err;
